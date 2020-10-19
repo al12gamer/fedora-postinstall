2e53e185a6044751de4c0b876696670195b8f6c7
Felipe Kinoshita <kinofhek@gmail.com>
2020-10-19 12:56:08 -0300

    feat: add flatpak list

--- fedora.sh
+++ fedora.sh
@@ -12,6 +12,11 @@ PACKAGE_LIST=(
 	hydrapaper
 )
 
+FLATPAK_LIST=(
+	discord
+	spotify	
+)
+
 # enable rpmfusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
@@ -33,6 +38,15 @@ sudo dnf update -y
 for package_name in ${PACKAGE_LIST[@]}; do
 	if ! sudo dnf list --installed | grep -q $package_name; then
 		sudo dnf install "$package_name" -y
+	else
+		echo "$package_name already installed"
+	fi
+done
+
+# iterate through packages and installs them if not already installed
+for flatpak_name in ${FLATPAK_LIST}; do
+	if ! flatpak list | grep -q $flatpak_name; then
+		flatpak install "$flatpak_name" -y
 	else
 		echo "$package_name already installed"
 	fi
