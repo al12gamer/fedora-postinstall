3c2aaaf6fe7ec062603a7f4e3f1c47d7defca577
Felipe Kinoshita <kinofhek@gmail.com>
2020-11-01 03:23:37 -0300

    fix: fix grep match when listing packages

---
+++ fedora.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+PACKAGE_LIST=(
+	vim
+	tilix
+	zsh
+	fira-code-fonts
+	lutris
+	akmod-nvidia
+	steam
+	geary
+	hydrapaper
+	code
+)
+
+FLATPAK_LIST=(
+	com.spotify.Client
+	info.febvre.Komikku
+	org.glimpse_editor.Glimpse
+	org.gnome.Podcasts
+	org.gnome.gitlab.somas.Apostrophe
+)
+
+# enable rpmfusion
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -yq
+
+sudo dnf groupupdate core -yq
+
+# install multimedia packages
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -yq
+
+sudo dnf groupupdate sound-and-video -yq
+
+# fedora better fonts
+sudo dnf copr enable dawid/better_fonts -yq
+sudo dnf install fontconfig-enhanced-defaults fontconfig-font-replacements -yq
+
+# add flathub repository
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# add third party software
+
+# vscode
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc --quiet
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+# update repositories
+
+sudo dnf check-update -yq
+
+# iterate through packages and installs them if not already installed
+for package_name in ${PACKAGE_LIST[@]}; do
+	if ! sudo dnf list --installed | grep -q ^$package_name; then
+		sudo dnf install "$package_name" -yq
+	else
+		echo "$package_name already installed"
+	fi
+done
+
+for flatpak_name in ${FLATPAK_LIST[@]}; do
+	if ! flatpak list | grep -q ^$flatpak_name; then
+		flatpak install "$flatpak_name" -y
+	else
+		echo "$package_name already installed"
+	fi
+done
+
+# upgrade packages
+sudo dnf upgrade -yq
+sudo dnf autoremove -yq
